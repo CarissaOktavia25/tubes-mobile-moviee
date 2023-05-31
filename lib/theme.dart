84410b42e2fe658ddd4ecb8c82a07b0d8a17182c
CarissaOktavia25 <karissaoktavia756@gmail.com>
2023-05-31 21:18:36 +0700

    tambah file theme

---
+++ lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+double defaultmargin = 30;
+
+
+Color bgcolor1 = const Color(0xffFFFFFF);
+Color bgcolor2 = const Color(0xff3A5BA0);
+Color primaryColor = const Color(0xff3A5BA0);
+Color primarytext = const Color(0xff3B3B3B);
+
+
+TextStyle primaryTextStyle = GoogleFonts.inter(
+  color: primarytext
+);
+
+
